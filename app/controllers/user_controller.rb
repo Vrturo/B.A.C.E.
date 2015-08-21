75d57f22f0a5aee62f043012ac06f7302064ad9f
Arturo Perez <VrturoPerez@gmail.com>
2015-08-20 21:09:41 -0700

    fix log in && log out

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -26,7 +26,7 @@ end
 
 post '/users/sessions' do
   user = User.find_by(email: params[:email])
-    if user.email && user.password_hash == params[:password]
+    if user.email && user.password == params[:password]
       set_session_n_redirect_home(user)
     else
       status 406
@@ -47,17 +47,28 @@ post '/users/calculate' do
   drinks = params[:drinks]
   weight = params[:weight]
   time = params[:time]
+  gender = params[:gender]
 
-  # @hours_drank = time * 0.015
-  # @volume = weight/2.2 * 0.58
-  # @bac = drinks/@volume - @hours_drank
+  #hours_drank = time * 0.015
+  #volume = weight/2.2 * 0.58
+  #bac = drinks/volume - hours_drank
 
-  ratio = divide(weight.to_f, 2.2)
-  volume = multiply(ratio.to_f, 0.58)
+  # male_ratio = divide(weight.to_f, 2.2)
+  # volume = multiply(male_ratio.to_f, 0.58)
+  # bac = divide(drinks.to_f, volume.to_f)
+  # bac_dropped = multiply(time.to_f, 0.015)
+  # final_bac_integer = subtract(bac.to_f, bac_dropped.to_f)
+  # @blood_alcohol_content = final_bac_integer.to_f.round(3)
+
+
+  female_ratio = divide(weight.to_f, 2.2)
+  volume = multiply(female_ratio.to_f, 0.58)
   bac = divide(drinks.to_f, volume.to_f)
-  p bac_dropped = multiply(time.to_f, 0.015)
-  p final_bac_integer = subtract(bac.to_f, bac_dropped.to_f)
+  bac_dropped = multiply(time.to_f, 0.015)
+  final_bac_integer = subtract(bac.to_f, bac_dropped.to_f)
   @blood_alcohol_content = final_bac_integer.to_f.round(3)
+
+
   erb :'/users/calculate'
 end
 
