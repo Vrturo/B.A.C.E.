ddf53a18826b06ddcc857489234daa22cd7d07b9
Arturo Perez <VrturoPerez@gmail.com>
2015-08-23 18:16:04 -0700

    add termsandconditions function

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,21 @@
 $( document ).ready(function() {
+  termsAndConditions();
+});
+
+termsAndConditions = function() {
   $('#terms').on('click', function (e){
     e.preventDefault();
   });
-});
-
-// termsAndConditions = function() {
-//   if ($('#agree').is(':checked')) {
-//     $(this).submit();
-//   };
-//   else {
-//     alert("You must agree with the terms and conditions to move on");
-//     return false;
-//   };
-// };
+  $('#terms').popup({ on: 'click'
+  });
+  $('#index_form_button').on('click', function(e){
+      e.preventDefault();
+     if ($('#agree').is(':checked')) {
+      $(this).submit();
+      }
+    else {
+      alert("You must agree with the terms and conditions to move on");
+      return false;
+    }
+  })
+};
