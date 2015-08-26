efcbdd333a92f3a898cb9d0d45ebe62a176c6ed7
Arturo Perez <VrturoPerez@gmail.com>
2015-08-25 18:31:14 -0700

    graph takes bac variable and updates

--- public/js/bar.js
+++ public/js/bar.js
@@ -2,11 +2,12 @@ var barData = {
     labels: ['Legal Limit', 'You'],
     datasets: [
         {
-            label: '2010 customers #',
+            label: 'Blood Alcohol Content',
             fillColor: '#900000',
-            data: [.087, .06]
+            data: [.08, json]
         }
     ]
 };
 var context = document.getElementById('myBar').getContext('2d');
 var clientsChart = new Chart(context).Bar(barData);
+var json = "#{@blood_alcohol_content || 'null'}"
