0623e97b48f694a59aef01a4ec510165c5b1ffbc
Arturo Perez <VrturoPerez@gmail.com>
2015-09-01 21:46:20 -0700

    api request dont work on calculate controller

--- app/controllers/calculate_controller.rb
+++ app/controllers/calculate_controller.rb
@@ -1,9 +1,28 @@
 post '/users/calculate' do
+
+  #unauthorized 2.0 credentials
     HEADER = {"Authorization" => "Bearer #{current_user.token}"}
 
-    p response = HTTParty.get("https://sandbox-api.uber.com/v1/products",
-      headers: HEADER)
 
+
+
+
+    p requests = HTTParty.post("https://sandbox-api.uber.com/v1/sandbox/requests/852b8fdd-4369-4659-9628-e122662ad257",
+        data:{
+          scope: "request",
+          product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d",
+          start_latitude: 37.777451,
+          start_longitude: 122.407577,
+          end_latitude: 37.784765,
+          end_longitude: 122.397524
+          }
+        )
+
+
+
+     # p map = HTTParty.get("https://sandbox-api.uber.com/v1/requests/852b8fdd-4369-4659-9628-e122662ad257/map",
+     #  headers: HEADER,
+     #  data: {scope: "request"})
 
 
     @calculation = Calculation.new(
